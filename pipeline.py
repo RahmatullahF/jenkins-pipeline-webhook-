43ade73b353669efa3c58b2d89269712a768fd4e
Rahmatullah <154547965+RahmatullahF@users.noreply.github.com>
2026-09-10 11:55:31 +0430

    Add print statements for Jenkins job
    Added multiple print statements for Jenkins job output.

--- pipeline.py
+++ pipeline.py
@@ -1,1 +1,12 @@
 print('My first python job which run within Jenkinsfile.')
+print('My first python job which run within Jenkinsfile.')
+
+print('My first python job which run within Jenkinsfile.')
+
+print('My first python job which run within Jenkinsfile.')
+
+print('My first python job which run within Jenkinsfile.')
+
+
+
+print('My Second python job which run within Jenkinsfile.')
